74340b8a2b6cdf05b6b528d521cedb0b554ab304
H00N24 <ondrej.kurak.h@gmail.com>
2018-12-14 08:20:55 +0100

    Output files rename + optimalization for aisa

--- src/main.h
+++ src/main.h
@@ -9,5 +9,5 @@
 #define TEST_DATA_FILE "../MNIST_DATA/mnist_test_vectors.csv"
 #define TEST_LABELS_FILE "../MNIST_DATA/mnist_test_labels.csv"
 
-#define TRAIN_LABELS_OUTPUT_FILE "../TrainPredictions"
-#define TEST_LABELS_OUTPUT_FILE "../TestPredictions"
+#define TRAIN_LABELS_OUTPUT_FILE "../trainPredictions"
+#define TEST_LABELS_OUTPUT_FILE "../actualTestPredictions"
